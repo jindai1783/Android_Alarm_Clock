c297b0b03abdb65a2a60a88eb28f035be25dab5a
Jin Dai <jindai1783@gmail.com>
2015-03-07 23:47:27 +0000

    Added the end activity and finish the app sequence

---
+++ app/src/main/java/com/serket/alarmclocktutorial/Bye.java
@@ -0,0 +1,12 @@
+package com.serket.alarmclocktutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Bye extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.bye_layout);
+    }
+}
